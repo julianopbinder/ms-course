14c10dec3f6bb018b35c152d0158beaeb7c3f6cd
Juliano Petrillo Binder <julianopbinder@gmail.com>
2021-05-13 00:25:27 -0300

    implementando projeto hr-worker

---
+++ hr-worker/src/main/java/com/binder/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,12 @@
+package com.binder.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.binder.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+	
+	
+	
+}
